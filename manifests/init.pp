3dcbbdc56e632f3dfea461702cd04ceeae34799e
root <root@liamjermy2.mylabserver.com>
2018-04-06 12:47:50 +0000

    Bug fix.

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,7 +11,7 @@ class ntp(
   String $package_name        = $ntp::params::package_name,
   String $package_ensure      = $ntp::params::package_ensure,
   String $config_name         = $ntp::params::config_name,
-  String $config_file_mode    = $ntp::params::config_file_more,
+  String $config_file_mode    = $ntp::params::config_file_mode,
   Array[String] $servers      = $ntp::params::servers,
   String $service_ensure      = $ntp::params::service_ensure,
   String $service_name        = $ntp::params::service_name,
